f48cfa357373b6d2375b3ef2a04d2d0192981333
Fortune <wulnyefortunatus@gmail.com>
2021-08-29 13:39:44 +0000

    adds solution

---
+++ IntroToMatlabForEngineer-WilliamJPalm3-SolutionToProblems/ch02/questionOne.m
@@ -0,0 +1,13 @@
+clc
+% Uncommnent the rest of the code one after the other to see display answers
+% a
+x = [5:0.2323:28] %or 
+% x = linspace(5,28,100);
+
+% b
+% x = [2:0.2:14] %or 
+% x = linspace(2,14,61)
+
+% c
+% x = [-2:1/7:5] %or
+% x = linspace(-2,5,50)
